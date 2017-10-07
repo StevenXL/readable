8c5edfaa559b0247dd08c3a2fd0d3bec2195fc17
Steven Leiva <leiva.steven@gmail.com>
2017-10-07 09:08:38 -0400

    Start migration to views.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
 import React, { Component } from "react";
-import { Grid, Row, Col } from "react-bootstrap";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
-import CategoriesListContainer from "./CategoriesListContainer";
-import PostsListContainer from "./PostsListContainer";
-import NewPost from "./NewPost";
+import DefaultView from "./DefaultView";
 
 class App extends Component {
   render() {
     return (
-      <Grid>
-        <Row>
-          <Col xs={12}>
-            <h1>Readable</h1>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col xs={12}>
-            <CategoriesListContainer />
-          </Col>
-        </Row>
-
-        <Row>
-          <Col xs={12}>
-            <NewPost />
-          </Col>
-        </Row>
-
-        <Row>
-          <Col xs={12}>
-            <Route path="/:category?" component={PostsListContainer} />
-          </Col>
-        </Row>
-      </Grid>
+      <Switch>
+        <Route exact path="/" component={DefaultView} />
+      </Switch>
     );
   }
 }
---
+++ src/DefaultView.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Grid, Row, Col } from "react-bootstrap";
+import { Route } from "react-router-dom";
+
+import CategoriesListContainer from "./CategoriesListContainer";
+import PostsListContainer from "./PostsListContainer";
+import NewPost from "./NewPost";
+
+const DefaultView = () => {
+  return (
+    <Grid>
+      <Row>
+        <Col xs={12}>
+          <h1>Readable</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <CategoriesListContainer />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <NewPost />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <Route path="/:category?" component={PostsListContainer} />
+        </Col>
+      </Row>
+    </Grid>
+  );
+};
+
+export default DefaultView;
